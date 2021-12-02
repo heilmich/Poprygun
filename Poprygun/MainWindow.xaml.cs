3357326af77e59089bb2041c46bd521d4b931c7f
HeilMich <uberheilmich@gmail.com>
2021-12-02 22:40:08 +0300

    4

--- Poprygun/MainWindow.xaml.cs
+++ Poprygun/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,9 @@ using System.Globalization;
 using System.ComponentModel;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
+using System.Drawing;
+
+
 
 namespace Poprygun
 {
@@ -25,14 +29,14 @@ namespace Poprygun
     /// </summary>
     /// 
 
-    public class SellsConverter : IValueConverter
+    public class SellsConverter : IValueConverter // конвертер расчёта кол-ва продаж для привязки
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             IEnumerable<ProductSale> ps = ((IEnumerable<ProductSale>)value).Where(p => p.SaleDate > DateTime.Now.AddDays(-365)); // не выводит продажи, так как последние продажи были в 2019
             if (ps.Count() == 0) return "Нет продаж за год";
-            else 
-            return ps.Count() + " продаж за год";
+            else
+                return ps.Count() + " продаж за год";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -42,12 +46,12 @@ namespace Poprygun
 
     }
 
-    public class DiscountConverter : IValueConverter
+    public class DiscountConverter : IValueConverter // конвертер скидки для привязки
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             HashSet<ProductSale> ps = (HashSet<ProductSale>)value;
-            var query = ps.Sum( p => p.ProductCount) * 1000;
+            var query = ps.Sum(p => p.ProductCount) * 1000;
 
             if (query >= 500000) return 25;
             else if (query >= 150000) return 20;
@@ -64,46 +68,85 @@ namespace Poprygun
 
     }
 
+    public class ImageConverter : IValueConverter // конвертер изображений для привязки
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return "picture.png";
+            
+            return MainWindow.DeserializeImage((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+
+    public class PageInfo
+    {
+        public int pageIndex = 1;                                   // индекс текущей страницы
+        public int pageSize = 10;                                   // размер страницы
+        public int totalItems;                                      // кол-во объектов
+        public int totalPages                                      // кол-во страниц
+        {
+            get { return (int)Math.Ceiling((decimal)totalItems / pageSize); }
+        }
+    }
+
     public partial class MainWindow : Window
     {
-        public static List<Agent> pageList;
-        public static List<Agent> agentList = new List<Agent>();
-        public Entities db = new Entities();
-
+        
+        public static List<Agent> agentList = new List<Agent>();    // список агентов
+        public Entities db = new Entities();                        // создание контекста базы данных
+        public PageInfo pageInfo = new PageInfo();           
+            
         
         public MainWindow()
         {
             InitializeComponent();
-            for (int i = 1; i <= 130; i++) 
-            {
-                SerializeImages(i);
-            }
-            
-            TakeAgentList();
-            UpdateList();
+            UpdatePage();
+            UpdatePage();
         }
 
-        public void TakeAgentList()
+        public async void TakeAgentList()
         {
-            var agents = from a in db.Agent
-                         select a;
-
-            foreach (var item in agents)
-            {
-                agentList.Add(item);
-            }
-        }
-
-        public void UpdateList()
-        {
+            await db.Agent.ForEachAsync(p =>
+               {
+                   agentList.Add(p);
+               });
             dataList.ItemsSource = agentList;
         }
 
+        public void UpdatePage() 
+        {
+            var currentList = TakeData();
+            // var currentList = agentList.Skip(pageInfo.pageIndex).Take(pageInfo.pageSize);
+
+            for (int i = pageInfo.pageIndex; i <= pageInfo.totalPages; i++) 
+            {
+                TextBlock page = new TextBlock();
+                page.Text = Convert.ToString(i);
+                pageList.Children.Add(page);
+                
+            }
+
+            dataList.ItemsSource = currentList;
+        }
+
+        public List<Agent> TakeData() 
+        {
+            return db.Agent.OrderBy( p => p.ID).Skip((pageInfo.pageIndex - 1) * pageInfo.pageSize).Take(pageInfo.pageSize).ToList();      
+        }
+
+
+
         public void SerializeImages(int num)
         {
-            string path = @"C:\Users\HeilMich\source\repos\Poprygun\agents\";
+            string path = @"D:\agents\";
             string pic;
             DirectoryInfo dirInfo = new DirectoryInfo(path);
+
             using (FileStream fs = File.OpenRead($"{path}agent_{num}.png"))
             {
                 byte[] array = new byte[fs.Length];
@@ -111,16 +154,19 @@ namespace Poprygun
 
                 pic = JsonSerializer.Serialize(array);
                 string search_str = "agent_" + Convert.ToString(num) + ".png";
-                using (Entities DB = new Entities())
+                using (Entities db = new Entities())
                 {
-                    Agent agent = DB.Agent.Where(p => p.Logo == search_str).FirstOrDefault();
+                    Agent agent = db.Agent.Where(p => p.Logo == search_str).FirstOrDefault();
                     if (agent == null) return;
-                    agent.Logo = pic;
-                    DB.SaveChanges();
+                    agent.Image = pic;
+                    
+                    db.SaveChanges();
                 }
                 
                 
             }
+
+
             /* string path2 = @"C:\Users\HeilMich\source\repos\Poprygun\";
             using (FileStream fs = File.Open(($"{path2}agent_{num}.png"), FileMode.OpenOrCreate))
             {
@@ -128,6 +174,19 @@ namespace Poprygun
                 array = JsonSerializer.Deserialize<byte[]>(pic);
                 fs.Write(array);
             } */
+        }
+
+        public static BitmapImage DeserializeImage(string value) 
+        {
+            byte[] array = System.Convert.FromBase64String(JsonSerializer.Deserialize<string>((string)value));
+            MemoryStream ms = new MemoryStream(array, 0, array.Length);
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.StreamSource = ms;
+            img.CacheOption = BitmapCacheOption.OnLoad;
+            img.EndInit();
+            img.Freeze();
+            return img;
         }
     }
 }
